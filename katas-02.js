6ea64b9582357621ecf9cdad06e078c5276c1b04
Arianna Basha <ariannagbasha@gmail.com>
2020-02-11 16:26:11 -0600

    Completed multiply and power finished

--- katas-02.js
+++ katas-02.js
@@ -5,51 +5,62 @@ function add(a,b) {
  
  }
 
- add(1,2);
+ add(2,4)
 
  function multiply(a,b) {
    let product = 0;
-   if(a < b){
-    for(let i = 1; i <= b; i++){
+  // if(a < b){
+    for(let i = 1; i <= b; i++){ // look at the less than
       
-     // product += a;
-     product = add(product, a);
+     //product += a;
+      product = add(product, a);
 
       
     }
 
-   } else {
-     for(let i = 1; i <= a; i++){
+//   // } else {
+//      for(let i = 1; i <= a; i++){
       
-      // product += b;
-      product = add(product, b);
-     }  
-   }
+//        product += b;
+//       // product = add(product, b);
+//      }  
+//    }
     
     return product
   }
- multiply(1,2);
+ multiply(6,8)
 
  function power(a,b) {
     let pow = 1;
-    if(a < b){
+  //  if(a < b){
         for(let i = 1; i <= b; i++){
           
-         // pow *= a;
-          pow = muliply(pow, a);
+        // pow *= a;
+          pow = multiply(pow, a);
     
           
         }
     
-       } else {
-         for(let i = 1; i <= a; i++){
+    //    } else {
+    //      for(let i = 1; i <= a; i++){
           
-          // pow *= b;
-           pow = multiply(pow, b);
-         }  
-       }
+    //        pow *= a;
+    //       // pow = multiply(pow, b);
+    //      }  
+    //    }
         
         return pow
  }
  
- power(8, 2)
+ power(3, 4)
+
+ function factorial(a) {
+     let fact = a;
+     for(let i = (a - 1) ; i >= 1; i--){
+         fact = multiply(fact, i)
+     //fact *= i;
+     }
+
+     return fact
+ }
+ factorial(4)
